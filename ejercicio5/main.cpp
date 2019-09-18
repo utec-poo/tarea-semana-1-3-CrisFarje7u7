757b336c5d01bb1f75244ada650c213b4772b496
CrisFarje7u7 <55298574+CrisFarje7u7@users.noreply.github.com>
2019-09-18 09:35:06 -0500

    Update main.cpp

--- ejercicio5/main.cpp
+++ ejercicio5/main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
-#include "Tipos.h"
 using namespace std;
 
 int main() {
-
-entero numero;
-caracter crt;
-
-cout << "ingrese el caracter:";
-cin >> crt;
-cout << "\ningrese el numero:";
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
+  int total=0, pares=0, impares=0;
+  int numero=-1;
+  cout<<"Ingrese los numeros: \n";
+  while (numero != 0 ){
+    cin>> numero;
+    if (numero!=0)
+      total++;
+      if (numero%2==0){
+      pares++;
+      }
+      else{
+      impares++;
+      }
+  }
+  cout << "El total de numeros es:"<<total<<"\nPares: "<<pares-1<<"\nImpares: "<<impares;
+  
   return 0;
 }
